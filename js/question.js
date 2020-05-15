83e3b2bae1d6b5b5feab33edbd50e931385e609a
Wijaya <57159833+RekkaWijaya@users.noreply.github.com>
2020-05-15 13:22:52 +0700

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "1. Dibawah ini yang merupakan unsur-unsur desain grafis yaitu .......",
+	"option1": "garis, bentuk, titik, koma ",
+	"option2": "garis, bentuk, warna, tekstur ",
+	"option3": "garis, bentuk, gelap terang, kotak ",
+	"option4": "warna, koma, bentuk, tekstur ",
+	"jawaban": "2"
+}, {
+	"question": "2. Rangkaian titik-titik yang tersusun dalam pola tertentu yang saling terhubung satu sama lain merupakan definisi dari ……….",
+	"option1": "tekstur ",
+	"option2": "bentuk",
+	"option3": "garis",
+	"option4": "warna",
+	"jawaban": "3"
+}, {
+	"question": "3. Tipe tekstur yang akan terlihat pola jika disandingkan secara berulang disebut ..........",
+	"option1": "Repetitive",
+	"option2": "Alami",
+	"option3": "Non-Repetitive",
+	"option4": "Masinal",
+	"jawaban": "1"
+}, {
+	"question": "4. Salah satu jenis olahan gambar dalam membuat illustrasi yang dibentuk oleh susunan raster/piksel/ titik koordinat dalam suatu grid disebut  ……… ",
+	"option1": "bitmap",
+	"option2": "komik",
+	"option3": "vektor",
+	"option4": "karikatur",
+	"jawaban": "1"
+}, {
+	"question": "5. Dalam ilmu memilih dan menata huruf terdapat jenis huruf yang memiliki kait, jenis huruf tersebut disebut  ……",
+	"option1": "san serif",
+	"option2": "monospace",
+	"option3": "serif",
+	"option4": "script",
+	"jawaban": "3"
+}, {
+	"question": "6. Ekstensi file corelDraw adalah ………",
+	"option1": ".CDR",
+	"option2": ".PSD",
+	"option3": ".AI",
+	"option4": ".JPG",
+	"jawaban": "1"
+}, {
+	"question": "7. Bagian yang berisi alat yang aktif (Active Tools) yang akan muncul jika pengguna mengklik tools tertentu atau membuat objek tertentu disebut ........",
+	"option1": "Menu bar",
+	"option2": "Title bar",
+	"option3": "Property bar",
+	"option4": "Tool bar",
+	"jawaban": "3"
+}, {
+	"question": "8. Polygon tool dapat digunakan untuk ............",
+	"option1": "membuat objek garis",
+	"option2": "membuat objek teks",
+	"option3": "membuat objek titik",
+	"option4": "membuat objek segitiga, segi empat, segi lima dan objek-objek segi banyak lainnya",
+	"jawaban": "4"
+}, {
+	"question": "9. …………. adalah tool untuk pembuatan berbagai model garis, kurva, dan menggambar bebas.",
+	"option1": "Ellips tool",
+	"option2": "Pick tool",
+	"option3": "Freehand tool",
+	"option4": "Crop tool",
+	"jawaban": "3"
+}, {
+	"question": "10. ……… adalah tool yang digunakan untuk pembuatan obyek lingkaran baik berupa lingkaran/oval, pie atau arc.",
+	"option1": "Pick tool",
+	"option2": "Ellips tool",
+	"option3": "Poligon tool",
+	"option4": "Ractangel tool",
+	"jawaban": "2"
+}];
